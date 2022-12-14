1e7b5ec94f87d42a0487fc74a9a6dd67e31df625
Abdurahim <abdurahimxakimov2@gmail.com>
2022-12-14 12:35:54 +0500

    updated by abdurahim

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
 from django.views.generic.list import ListView
@@ -7,7 +7,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormVi
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import Login
+from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -20,6 +20,11 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['task'] = context['task'].filter(user=self.request.user)
         context['count'] = context['task'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['task'] = context['task'].filter(title__icontains=search_input)
+            context['search_input'] = search_input
         return context
 
 
@@ -60,3 +65,21 @@ class UserLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('task')
+
+
+class RegisterPage(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    redirect_authenticated = True
+    success_url = reverse_lazy('task')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('task')
+        return super(RegisterPage, self).get(*args, **kwargs)
